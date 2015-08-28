1441ccf8823ac7fb0ace82066924c8616dbbbca9
vdbvincent <vincent.vdb@gmail.com>
2015-08-28 10:18:12 +0200

    Ajout gestionnaire de logs

---
+++ logs.h
@@ -0,0 +1,18 @@
+/*
+* logs.h
+*
+* Created on: 31 juil. 2015
+* Author: vincent
+*/
+#ifndef _LOGS_H_
+#define _LOGS_H_
+
+#include "common.h"
+#include "ctrleur.h"
+
+
+void logs_setup(void);
+void logs_every10ms(void);
+
+
+#endif /* _LOGS_H_ */
